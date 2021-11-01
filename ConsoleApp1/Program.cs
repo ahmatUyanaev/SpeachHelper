b5a0cccb0faa72481ce36b8bc0389fb683508e72
A-UYANAEV <A-UYANAEV@DESKTOP-29CFBJD>
2021-11-02 00:21:40 +0300

    add ironOcr test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using IronOcr;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("form.png"))
+            {
+
+            }
+            var Ocr = new IronTesseract();
+            Ocr.Language = OcrLanguage.Russian;
+
+            using (var Input = new OcrInput("vk.png"))
+            {
+                var Result = Ocr.Read(Input);
+                Console.WriteLine(Result.Text);
+            }
+           
+        }
+    }
+}
---
+++ SpeachHelper.Common/OCR/OcrEngine.cs
@@ -0,0 +1,23 @@
+﻿using IronOcr;
+using System;
+
+namespace SpeachHelper.Common.OCR
+{
+    public class OcrEngine
+    {
+        public OcrEngine()
+        {
+
+            var Ocr = new IronTesseract();
+            Ocr.Language = OcrLanguage.Russian;
+
+            using (var Input = new OcrInput("vk.png"))
+            {
+                var Result = Ocr.Read(Input);
+                Console.WriteLine(Result.Text);
+            }
+
+
+        }
+    }
+}
