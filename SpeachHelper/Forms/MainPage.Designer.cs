738d0007463e539fb90a43ce7387f547860ebe88
A-UYANAEV <A-UYANAEV@DESKTOP-29CFBJD>
2021-11-14 02:17:13 +0300

    delete tab

--- SpeachHelper/Forms/MainPage.Designer.cs
+++ SpeachHelper/Forms/MainPage.Designer.cs
@@ -72,7 +72,7 @@ namespace SpeachHelper
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(794, 451);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Редактировать команды";
+            this.tabPage2.Text = "Команды";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // deleteCommandBtn
